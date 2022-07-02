f4e09a8036c45f8d263eb4c5c5948706b1d506be
Stéphane Gully <gully@abes.fr>
2022-07-02 17:18:47 +0200

    feat: parametrage du mot de passe elastic et du host d'elastic pour le batch

--- images/theses-batch/theses-sample-load.sh
+++ images/theses-batch/theses-sample-load.sh
@@ -2,19 +2,19 @@
 
 echo "-> Suppression de l'eventuel ancien index theses-sample dans elasticsearch"
 curl -s -k --request DELETE \
-  -u elastic:thesessecret2 \
-  --url 'https://theses-elasticsearch-es01:9200/theses-sample?pretty=true'
+  -u elastic:${ELASTIC_PASSWORD} \
+  --url "${ELASTICSEARCH_HOST}/theses-sample?pretty=true"
 
 echo "-> Création du mapping de l'index theses-sample dans elasticsearch"
 cat theses-sample-mapping.json | curl -s -k --request PUT \
-  -u elastic:thesessecret2 \
-  --url 'https://theses-elasticsearch-es01:9200/theses-sample?pretty=true' \
+  -u elastic:${ELASTIC_PASSWORD} \
+  --url "${ELASTICSEARCH_HOST}/theses-sample?pretty=true" \
   --header 'Content-Type: application/json' \
   --data-binary @-
 
 echo "-> Chargement des documents dans l'index theses-sample d'elasticsearch"
 cat theses-sample-data.json | jq -c '.[]' | curl -s -k --request POST \
-  -u elastic:thesessecret2 \
-  --url 'https://theses-elasticsearch-es01:9200/_bulk/?pretty=true' \
+  -u elastic:${ELASTIC_PASSWORD} \
+  --url "${ELASTICSEARCH_HOST}/_bulk/?pretty=true" \
   --header 'Content-Type: application/x-ndjson' \
   --data-binary @-
